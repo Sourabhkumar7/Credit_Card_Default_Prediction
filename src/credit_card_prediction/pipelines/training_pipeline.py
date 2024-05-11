b6e6aea1561c1040da0d9ff52b5d296407c5b89b
Sourabhkumar7 <sourabhkumar9221@gmail.com>
2024-05-11 18:54:21 +0530

    pipeline created

---
+++ src/credit_card_prediction/pipelines/training_pipeline.py
@@ -0,0 +1,30 @@
+from src.credit_card_prediction.components.data_ingestion import DataIngestion
+
+from src.credit_card_prediction.components.data_transformation import DataTransformation
+
+from src.credit_card_prediction.components.model_trainer import ModelTrainer
+
+#from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
+
+
+import os
+import sys
+from src.credit_card_prediction.logger import logging
+from src.credit_card_prediction.exception import customexception
+import pandas as pd
+
+obj=DataIngestion()
+
+train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+data_transformation=DataTransformation()
+
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr,test_arr)
+
+
+#model_eval_obj = ModelEvaluation()
+#model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
