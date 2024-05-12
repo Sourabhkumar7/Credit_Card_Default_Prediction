14f22934c3eae59377d07ddc6dcddfb5ab1c1b05
Sourabhkumar7 <sourabhkumar9221@gmail.com>
2024-05-12 22:52:16 +0530

    url created using Flask

--- app.py
+++ app.py
@@ -20,31 +20,30 @@ def predict_datapoint():
     else:
         data=CustomData(
             
-            
-        limit_bal = request.form.get("LIMIT_BAL"),
-        sex = request.form.get("SEX"),
-        education = request.form.get("EDUCATION"),
-        marriage = request.form.get("MARRIAGE"),
-        age = request.form.get("AGE"),
-        pay_0 = request.form.get("PAY_0"),
-        pay_2 = request.form.get("PAY_2"),
-        pay_3 = request.form.get("PAY_3"),
-        pay_4 = request.form.get("PAY_4"),
-        pay_5 = request.form.get("PAY_5"),
-        pay_6 = request.form.get("PAY_6"),
-        bill_amt1 = request.form.get("BILL_AMT1"),
-        bill_amt2 = request.form.get("BILL_AMT2"),
-        bill_amt3 = request.form.get("BILL_AMT3"),
-        bill_amt4 = request.form.get("BILL_AMT4"),
-        bill_amt5 = request.form.get("BILL_AMT5"),
-        bill_amt6 = request.form.get("BILL_AMT6"),
-        pay_amt1 = request.form.get("PAY_AMT1"),
-        pay_amt2 = request.form.get("PAY_AMT2"),
-        pay_amt3 = request.form.get("PAY_AMT3"),
-        pay_amt4 = request.form.get("PAY_AMT4"),
-        pay_amt5 = request.form.get("PAY_AMT5"),
-        pay_amt6 = request.form.get("PAY_AMT6")
-        )
+        limit_bal = request.form.get("limit_bal"),  
+        sex = request.form.get("sex"),
+        education = request.form.get("education"),
+        marriage = request.form.get("marriage"),
+        age = request.form.get("age"),
+        pay_0 = request.form.get("pay_0"),
+        pay_2 = request.form.get("pay_2"),
+        pay_3 = request.form.get("pay_3"),
+        pay_5 = request.form.get("pay_5"),
+        pay_4 = request.form.get("pay_4"),
+        pay_6 = request.form.get("pay_6"),
+        bill_amt1 = request.form.get("bill_amt1"),
+        bill_amt2 = request.form.get("bill_amt2"),
+        bill_amt3 = request.form.get("bill_amt3"),
+        bill_amt4 =request.form.get("bill_amt4"),
+        bill_amt5 = request.form.get("bill_amt5"),
+        bill_amt6 = request.form.get("bill_amt6"),
+        pay_amt1 = request.form.get("pay_amt1"),
+        pay_amt2 = request.form.get("pay_amt2"),
+        pay_amt3 = request.form.get("pay_amt3"),
+        pay_amt4 = request.form.get("pay_amt4"),
+        pay_amt5 = request.form.get("pay_amt5"),
+        pay_amt6 = request.form.get("pay_amt6")
+)        
          #this is my final data
         final_data=data.get_data_as_dataframe()
         
@@ -56,11 +55,15 @@ def predict_datapoint():
         if result==1:
             result="The user is default from next month"
         else:
-            result="The user is not default"
+            result="THE USER IS NOT DEFAULT"
+    # if round(pred[0],2) == 1:
+    #     result = "The user is default from next month"
+    # else:
+    #     result = "The user is not default"
         
         return render_template("result.html",final_result=result)
 
 #execution begin
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
-    #app.run(host="0.0.0.0",port=8080)
+    #app.run(debug=True,port=5000)
+    app.run(host="0.0.0.0",port=8080)
